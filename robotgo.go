c9b0b69650c301226c7ada8abe4905fd4b9b7467
vCaesar <vzvway@gmail.com>
2016-11-10 19:27:02 +0800

    Add LICENSE

--- robotgo.go
+++ robotgo.go
@@ -1,3 +1,13 @@
+// Copyright 2016 The go-vgo Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://www.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 package robotgo
 
 /*
@@ -14,8 +24,8 @@ package robotgo
 #include "mouse/goMouse.h"
 #include "key/goKey.h"
 //#include "bitmap/goBitmap.h"
-//#include "window/goWindow.h"
 //#include "event/goEvent.h"
+//#include "window/goWindow.h"
 */
 import "C"
 
