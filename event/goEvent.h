1c514664328128982cb5ec692b11605ca6fc3359
vCaesar <vzvway@gmail.com>
2017-03-17 19:49:54 +0800

    Fix typesetting

--- event/goEvent.h
+++ event/goEvent.h
@@ -7,8 +7,9 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+//
 // The hook directory link from the https://github.com/robotn/gohook/
-// event/hook,You need to follow the relevant agreement and LICENSE.
+// event/hook, you need to follow the relevant agreement and LICENSE.
 // See the LICENSE file at the top-level directory of this distribution and at
 // https://github.com/robotn/gohook/blob/master/LICENSE
 
