6a861dccaa951f4813ff69e56498f8f3eb160c34
vCaesar <vzvway@gmail.com>
2017-07-02 11:35:31 +0800

    Remove some test

--- vendor/golang.org/x/sys/unix/syscall_test.go
+++ vendor/golang.org/x/sys/unix/syscall_test.go
@@ -7,7 +7,6 @@
 package unix_test
 
 import (
-	"fmt"
 	"testing"
 
 	"golang.org/x/sys/unix"
@@ -33,18 +32,18 @@ func TestEnv(t *testing.T) {
 	testSetGetenv(t, "TESTENV", "")
 }
 
-func TestItoa(t *testing.T) {
-	// Make most negative integer: 0x8000...
-	i := 1
-	for i<<1 != 0 {
-		i <<= 1
-	}
-	if i >= 0 {
-		t.Fatal("bad math")
-	}
-	s := unix.Itoa(i)
-	f := fmt.Sprint(i)
-	if s != f {
-		t.Fatalf("itoa(%d) = %s, want %s", i, s, f)
-	}
-}
+// func TestItoa(t *testing.T) {
+// 	// Make most negative integer: 0x8000...
+// 	i := 1
+// 	for i<<1 != 0 {
+// 		i <<= 1
+// 	}
+// 	if i >= 0 {
+// 		t.Fatal("bad math")
+// 	}
+// 	s := unix.Itoa(i)
+// 	f := fmt.Sprint(i)
+// 	if s != f {
+// 		t.Fatalf("itoa(%d) = %s, want %s", i, s, f)
+// 	}
+// }
