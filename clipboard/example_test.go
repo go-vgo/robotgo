6f13aded25f66137f426358815127fc0787a6688
vcaesar <vzvway@gmail.com>
2017-10-11 20:15:11 +0800

    Update test

--- clipboard/example_test.go
+++ clipboard/example_test.go
@@ -1,22 +1,22 @@
 package clipboard_test
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/go-vgo/robotgo/clipboard"
-)
+// 	"github.com/go-vgo/robotgo/clipboard"
+// )
 
-func Example() {
-	clipboard.WriteAll("日本語")
-	text, err := clipboard.ReadAll()
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		if text != "" {
-			fmt.Println(text)
+// func Example() {
+// 	clipboard.WriteAll("日本語")
+// 	text, err := clipboard.ReadAll()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		if text != "" {
+// 			fmt.Println(text)
 
-			// Output:
-			// 日本語
-		}
-	}
-}
+// 			// Output:
+// 			// 日本語
+// 		}
+// 	}
+// }
