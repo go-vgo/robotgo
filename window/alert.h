8b1854cac7bf819a52dcf0b10218c781def43c72
vcaesar <vzvway@gmail.com>
2018-03-02 23:08:12 +0800

    update code style

--- window/alert.h
+++ window/alert.h
@@ -11,7 +11,7 @@
 /* Displays alert with given attributes, and blocks execution until the user
  * responds. Returns 0 if defaultButton was pressed, 1 if cancelButton was
  * pressed, or -1 if an error occurred. */
-int showAlert(const char *title, const char *msg, const char *defaultButton,
-              const char *cancelButton);
+int showAlert(const char *title, const char *msg,
+	const char *defaultButton, const char *cancelButton);
 
 #endif /* ALERT_H */
