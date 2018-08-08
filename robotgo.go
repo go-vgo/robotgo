01bee868bbf0f351a88ae32a5c31d2cef6b82699
vcaesar <vzvway@gmail.com>
2018-08-08 09:43:46 -0400

    v0.50.0 is released!

--- robotgo.go
+++ robotgo.go
@@ -70,7 +70,7 @@ import (
 
 const (
 	// Version get the robotgo version
-	Version string = "v0.50.0.640, Olympus Mons!"
+	Version string = "v0.50.0.647, The Appalachian Mountains!"
 )
 
 // GetVersion get the robotgo version
