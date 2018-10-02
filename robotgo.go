51a2394a94c60beff033cef2ef5ef88bc7f2ccda
vz <vzvway@gmail.com>
2018-10-02 09:05:57 -0400

    update godoc

--- robotgo.go
+++ robotgo.go
@@ -1565,7 +1565,7 @@ func FindNames() ([]string, error) {
 	return strArr, err
 }
 
-// FindIds finds the all processes named with a subset of "name" (case insensitive), returning matching IDs.
+// FindIds finds the all processes named with a subset of "name" (case insensitive), return matching IDs.
 func FindIds(name string) ([]int32, error) {
 	var pids []int32
 	nps, err := Process()
