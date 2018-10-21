83845a8488ffbc5ffa2789124283c0f400457145
vcaesar <vzvway@gmail.com>
2018-10-21 12:48:34 -0400

    add color picker and get mouse pos example

---
+++ examples/color-picker/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func colorPicker() {
+	m := robotgo.AddEvent("mleft")
+	if m == 0 {
+		x, y := robotgo.GetMousePos()
+		fmt.Println("mouse pos: ", x, y)
+
+		clo := robotgo.GetPixelColor(x, y)
+		fmt.Println("color: #", clo)
+	}
+}
+
+func main() {
+	fmt.Println("color picker...")
+
+	colorPicker()
+}
