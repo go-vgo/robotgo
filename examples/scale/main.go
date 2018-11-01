f4cff13c0a4f6f5ecd1767daca7a13da215fc6cf
vcaesar <vzvway@gmail.com>
2018-11-01 15:10:58 -0400

    update scale example code close #153

--- examples/scale/main.go
+++ examples/scale/main.go
@@ -7,15 +7,20 @@ import (
 )
 
 func main() {
+	//
+	// syscall.NewLazyDLL("user32.dll").NewProc("SetProcessDPIAware").Call()
+
 	width, height := robotgo.GetScaleSize()
-	fmt.Println("GetScreenSize: ", width, height)
+	fmt.Println("get scale screen size: ", width, height)
 
 	bitmap := robotgo.CaptureScreen(0, 0, width, height)
 	robotgo.SaveBitmap(bitmap, "test.png")
 
+	sx := robotgo.ScaleX()
 	s := robotgo.Scale()
 	robotx := 35 * s / 100
 	roboty := 25 * s / 100
+	fmt.Println("scale: ", sx, s, " pos: ", robotx, roboty)
 
 	bit1 := robotgo.CaptureScreen(0, 0, robotx, roboty)
 	robotgo.SaveBitmap(bit1, "test2.png")
