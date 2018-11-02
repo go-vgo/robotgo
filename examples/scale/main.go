b6ddb6cb1112b77439b139a4b21f43eb8dd95987
vcaesar <vzvway@gmail.com>
2018-11-02 13:17:42 -0400

    add more scale example

--- examples/scale/main.go
+++ examples/scale/main.go
@@ -18,11 +18,15 @@ func main() {
 
 	sx := robotgo.ScaleX()
 	s := robotgo.Scale()
-	robotx := 35 * s / 100
-	roboty := 25 * s / 100
+	robotx, roboty := 35*s/100, 25*s/100
 	fmt.Println("scale: ", sx, s, " pos: ", robotx, roboty)
 
-	bit1 := robotgo.CaptureScreen(0, 0, robotx, roboty)
+	mx, my := robotgo.GetMousePos()
+	sx, sy := mx*s/100, my*s/100
+
+	rx, ry, rw, rh := sx, sy, robotx, roboty
+	// bit1 := robotgo.CaptureScreen(10, 20, robotw, roboth)
+	bit1 := robotgo.CaptureScreen(rx, ry, rw, rh)
 	robotgo.SaveBitmap(bit1, "test2.png")
 
 	clo := robotgo.GetPixelColor(robotx, roboty)
