e11eccdd3f739febd013016a85ceb9e9e47997ee
vcaesar <vzvway@gmail.com>
2019-04-23 13:35:39 -0400

    add robotgo simple test code

---
+++ robotgo_test.go
@@ -0,0 +1,34 @@
+// Copyright 2016 The go-vgo Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// https://github.com/go-vgo/robotgo/blob/master/LICENSE
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+package robotgo
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/vcaesar/tt"
+)
+
+func TestGetVer(t *testing.T) {
+	fmt.Println("go version: ", runtime.Version())
+	ver := GetVersion()
+
+	tt.Expect(t, Version, ver)
+}
+
+func TestMouse(t *testing.T) {
+	MoveMouse(10, 10)
+	x, y := GetMousePos()
+
+	tt.Equal(t, 10, x)
+	tt.Equal(t, 10, y)
+}
