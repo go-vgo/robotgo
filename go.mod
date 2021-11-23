fb06a2929205cd76c4478ac83c262fe016a14e2a
vcaesar <vzvway@gmail.com>
2021-11-23 16:30:24 -0400

    Update keycode.go use the public code and go mod

--- go.mod
+++ go.mod
@@ -8,23 +8,24 @@ require (
 	github.com/robotn/gohook v0.31.2
 	github.com/robotn/xgb v0.0.0-20190912153532-2cb92d044934
 	github.com/robotn/xgbutil v0.0.0-20190912154524-c861d6f87770
-	github.com/vcaesar/gops v0.21.2
+	github.com/vcaesar/gops v0.21.3
 	github.com/vcaesar/imgo v0.30.0
+	github.com/vcaesar/keycode v0.10.0
 	github.com/vcaesar/tt v0.20.0
-	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
 )
 
 require (
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-ole/go-ole v1.2.5 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/otiai10/mint v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/shirou/gopsutil v3.21.8+incompatible // indirect
+	github.com/shirou/gopsutil v3.21.10+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.9 // indirect
 	github.com/tklauser/numcpus v0.3.0 // indirect
-	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
+	golang.org/x/sys v0.0.0-20211123173158-ef496fb156ab // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
--- keycode.go
+++ keycode.go
@@ -10,142 +10,17 @@
 
 package robotgo
 
+import (
+	"github.com/vcaesar/keycode"
+)
+
 type uMap map[string]uint16
 
 // MouseMap robotgo hook mouse's code map
-var MouseMap = uMap{
-	"left":       1,
-	"right":      2,
-	"center":     3,
-	"wheelDown":  4,
-	"wheelUp":    5,
-	"wheelLeft":  6,
-	"wheelRight": 7,
-}
+var MouseMap = keycode.MouseMap
 
 // Keycode robotgo hook key's code map
-var Keycode = uMap{
-	"`": 41,
-	"1": 2,
-	"2": 3,
-	"3": 4,
-	"4": 5,
-	"5": 6,
-	"6": 7,
-	"7": 8,
-	"8": 9,
-	"9": 10,
-	"0": 11,
-	"-": 12,
-	"=": 13,
-	//
-	"_": 12,
-	"+": 13,
-	//
-	"q":  16,
-	"w":  17,
-	"e":  18,
-	"r":  19,
-	"t":  20,
-	"y":  21,
-	"u":  22,
-	"i":  23,
-	"o":  24,
-	"p":  25,
-	"[":  26,
-	"]":  27,
-	"\\": 43,
-	//
-	"{": 26,
-	"}": 27,
-	"|": 43,
-	//
-	"a": 30,
-	"s": 31,
-	"d": 32,
-	"f": 33,
-	"g": 34,
-	"h": 35,
-	"j": 36,
-	"k": 37,
-	"l": 38,
-	";": 39,
-	"'": 40,
-	//
-	":": 39,
-	`"`: 40,
-	//
-	"z": 44,
-	"x": 45,
-	"c": 46,
-	"v": 47,
-	"b": 48,
-	"n": 49,
-	"m": 50,
-	",": 51,
-	".": 52,
-	"/": 53,
-	//
-	"<": 51,
-	">": 52,
-	"?": 53,
-	//
-	"f1":  59,
-	"f2":  60,
-	"f3":  61,
-	"f4":  62,
-	"f5":  63,
-	"f6":  64,
-	"f7":  65,
-	"f8":  66,
-	"f9":  67,
-	"f10": 68,
-	"f11": 69,
-	"f12": 70,
-	// more
-	"esc":     1,
-	"delete":  14,
-	"tab":     15,
-	"enter":   28,
-	"ctrl":    29,
-	"control": 29,
-	"shift":   42,
-	"rshift":  54,
-	"space":   57,
-	//
-	"alt":     56,
-	"ralt":    3640,
-	"cmd":     3675,
-	"command": 3675,
-	"rcmd":    3676,
-	//
-	"up":    57416,
-	"down":  57424,
-	"left":  57419,
-	"right": 57421,
-}
+var Keycode = keycode.Keycode
 
 // Special is the special key map
-var Special = map[string]string{
-	"~": "`",
-	"!": "1",
-	"@": "2",
-	"#": "3",
-	"$": "4",
-	"%": "5",
-	"^": "6",
-	"&": "7",
-	"*": "8",
-	"(": "9",
-	")": "0",
-	"_": "-",
-	"+": "=",
-	"{": "[",
-	"}": "]",
-	"|": "\\",
-	":": ";",
-	`"`: "'",
-	"<": ",",
-	">": ".",
-	"?": "/",
-}
+var Special = keycode.Special
