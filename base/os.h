8defbda0cdebd749043f98222f0156872ec49af3
vcaesar <vzvway@gmail.com>
2022-01-22 12:53:04 -0400

    Move type def to type.h

--- base/os.h
+++ base/os.h
@@ -60,12 +60,4 @@
 	#endif
 #endif
 
-#ifdef RobotGo_64
-	typedef int64_t			intptr;	
-	typedef uint64_t		uintptr;	
-#else
-	typedef int32_t			 intptr;
-	typedef uint32_t		 uintptr;	// Unsigned pointer integer
-#endif
-
 #endif /* OS_H */
