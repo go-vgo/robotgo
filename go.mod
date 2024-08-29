fc03362e0ca03a8d37b00701290d0c878e0b7814
Jaroslav Jacjuk <j.jacjuk@semrush.com>
2024-08-29 21:54:53 +0200

    Modified RobotGo to avoid github.com/kbinani/screenshot with deprecated CGDisplayCreateImageForRect on macOS 15

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/yarda7/robotgo
 
-go 1.17
+go 1.21
+
+toolchain go1.21.0
 
 require (
 	// github.com/robotn/gohook v0.31.3
@@ -15,15 +17,16 @@ require (
 
 require (
 	github.com/go-vgo/robotgo v0.110.2
+	github.com/kbinani/screenshot v0.0.0-20240820160931-a8a2c5d0e191
 	github.com/otiai10/gosseract v2.2.1+incompatible
 )
 
 require (
 	github.com/dblohm7/wingoes v0.0.0-20231019175336-f6e33aa7cc34 // indirect
-	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
+	github.com/gen2brain/shm v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
-	github.com/jezek/xgb v1.1.0 // indirect
-	github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/lufia/plan9stats v0.0.0-20230326075908-cb1d2100619a // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/otiai10/mint v1.6.3 // indirect
@@ -35,7 +38,7 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/image v0.18.0 // indirect
 	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 )
 
 // replace golang.org/x/sys => github.com/golang/sys v0.0.0-20190109145017-48ac38b7c8cb
---
+++ screen/screen_c.h
@@ -0,0 +1,73 @@
+//#include "../base/os.h"
+
+#if defined(IS_MACOSX)
+	#include <ApplicationServices/ApplicationServices.h>
+#elif defined(USE_X11)
+	#include <X11/Xlib.h>
+	// #include "../base/xdisplay_c.h"
+#endif
+
+MMSizeInt32 getMainDisplaySize(void) {
+#if defined(IS_MACOSX)
+	CGDirectDisplayID displayID = CGMainDisplayID();
+	CGRect displayRect = CGDisplayBounds(displayID);
+	
+	CGSize size = displayRect.size;
+	return MMSizeInt32Make((int32_t)size.width, (int32_t)size.height);
+#elif defined(USE_X11)
+	Display *display = XGetMainDisplay();
+	const int screen = DefaultScreen(display);
+
+	return MMSizeInt32Make(
+						(int32_t)DisplayWidth(display, screen),
+	                	(int32_t)DisplayHeight(display, screen));
+#elif defined(IS_WINDOWS)
+	return MMSizeInt32Make(
+ 						(int32_t)GetSystemMetrics(SM_CXSCREEN),
+ 		                (int32_t)GetSystemMetrics(SM_CYSCREEN));
+#endif
+}
+
+MMRectInt32 getScreenRect(int32_t display_id) {
+#if defined(IS_MACOSX)
+	CGDirectDisplayID displayID = (CGDirectDisplayID) display_id;
+	if (display_id == -1) {
+		displayID = CGMainDisplayID();
+	}
+	CGRect displayRect = CGDisplayBounds(displayID);
+
+	CGPoint point = displayRect.origin;
+	CGSize size = displayRect.size;
+	return MMRectInt32Make(
+		(int32_t)point.x, (int32_t)point.y,
+		(int32_t)size.width, (int32_t)size.height);
+#elif defined(USE_X11)
+	Display *display = XGetMainDisplay();
+	const int screen = DefaultScreen(display);
+
+	return MMRectInt32Make(
+					(int32_t)0, (int32_t)0,
+					(int32_t)DisplayWidth(display, screen),
+	                (int32_t)DisplayHeight(display, screen));
+#elif defined(IS_WINDOWS)
+	if (GetSystemMetrics(SM_CMONITORS) == 1 
+			|| display_id == -1 || display_id == 0) {
+ 		return MMRectInt32Make(
+						(int32_t)0,
+						(int32_t)0,
+			 			(int32_t)GetSystemMetrics(SM_CXSCREEN),
+ 		                (int32_t)GetSystemMetrics(SM_CYSCREEN));
+ 	} else {
+ 		return MMRectInt32Make(
+			 			(int32_t)GetSystemMetrics(SM_XVIRTUALSCREEN),
+						(int32_t)GetSystemMetrics(SM_YVIRTUALSCREEN),
+						(int32_t)GetSystemMetrics(SM_CXVIRTUALSCREEN),
+ 		                (int32_t)GetSystemMetrics(SM_CYVIRTUALSCREEN));
+ 	}
+#endif
+}
+
+bool pointVisibleOnMainDisplay(MMPointInt32 point){
+	MMSizeInt32 displaySize = getMainDisplaySize();
+	return point.x < displaySize.w && point.y < displaySize.h;
+}
